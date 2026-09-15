d1d1124606141567c96aea98fe41f81e68b31347
kadoka <tomiya3499@gmail.com>
2026-09-16 07:44:05 +0900

    Export versus Pygame adapter

--- src/tetris/adapters/__init__.py
+++ src/tetris/adapters/__init__.py
@@ -2,4 +2,6 @@ from .jsonl import decode_commands, decode_input_action, encode_event
 from .jsonl_api_server import JsonlApiServer
 from .keyboard import action_for_key
 from .pygame_app import PygameApp
+from .pygame_versus_app import PygameVersusApp
+from .pygame_versus_view import PygameVersusView
 from .pygame_view import PygameView
