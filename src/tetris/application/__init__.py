9192da9d3eba5e3d72e4964cc3f0c5b9c461865b
kadoka <tomiya3499@gmail.com>
2026-09-16 19:44:37 +0900

    Export common AI backend and runner APIs

--- src/tetris/application/__init__.py
+++ src/tetris/application/__init__.py
@@ -1,4 +1,14 @@
+from .ai_backend import (
+    AIBackend,
+    AIBackendKind,
+    AIProposal,
+    FunctionAIBackend,
+    NativePlannerBackend,
+    PersistentProcessAIBackend,
+    public_observation,
+)
 from .ai_match import AIMatchRunner
+from .ai_runner import AITurnResult, run_ai_turn
 from .attack import attack_for_clear, attack_for_event, cancel_attack
 from .command import Command
 from .input_action import InputAction, SEMANTIC_ACTIONS
